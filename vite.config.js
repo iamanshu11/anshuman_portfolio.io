f7851f3a8e2f9ca377a17b0b9932f853fa548693
iamanshu11 <anshuman.raj@accropolix.live>
2025-03-08 20:33:37 +0530

    content updated

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/anshuman_portfolio.io/',
     plugins: [vue(),
         tailwindcss(),
     ],
